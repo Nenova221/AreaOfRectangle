595aa1d1dc0da4a063bac59ef8731345687baef6
Nenova221 <kalina.nenova@paisii.info>
2024-08-20 20:28:43 +0300

    First commit

---
+++ AreaOfRectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfRectangle
+{
+    public class AreaOfRectangle
+    {
+        public static int CalculateArea(int a,  int b)
+        {
+            Console.Write("Enter side a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter side b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The area of the rectangle is: ", a * b);
+            return AreaOfRectangle.CalculateArea(a, b);
+        }
+    }
+}
